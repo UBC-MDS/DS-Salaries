24a067997a96471639f25748baaeee039b1266b1
Ruslan Dimitrov <ruslan.d.dim@gmail.com>
2023-03-13 18:32:04 -0700

    make no-selection to be average of all salaries in salary by year

--- app.R
+++ app.R
@@ -284,6 +284,14 @@ server <- function(input, output, session) {
         experience_level == "EX" ~ "Executive-level / Director",
         TRUE ~ experience_level
       ))
+    if (length(input$exp_levels) == 0){
+      ggplot2::ggplot(data_salaries_filtered, aes(x = work_year, y = salary_in_usd/1000)) +
+        ggplot2::geom_line(stat = "summary", fun = "mean", color = 'green') +
+        ggplot2::xlab("Work Year") +
+        ggplot2::ylab("Average Salary in '000 USD") +
+        ggplot2::ggtitle("Average Salary per Year by All Experience Levels")
+      
+    } else {
     ggplot2::ggplot(data_salaries_filtered, aes(x = work_year, y = salary_in_usd/1000, color = experience_level)) +
       ggplot2::geom_line(stat = "summary", fun = "mean") +
       ggplot2::xlab("Work Year") +
@@ -295,6 +303,7 @@ server <- function(input, output, session) {
                                     "Executive-level / Director" = "red"),
                          name = "Experience Level"
       )
+    }
   })
   
   
